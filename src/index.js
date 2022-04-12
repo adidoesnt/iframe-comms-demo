6d2813c058ed352f36757602de0b39ba0c6fb260
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-04-12 14:50:55 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+function init() {
+    const mainContainerElement = document.createElement('div');
+    mainContainerElement.setAttribute('class', 'main-container');
+    document.body.appendChild(mainContainerElement);
+}
+
+init();
